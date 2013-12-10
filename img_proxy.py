f1959247b70dd5dc03096521418aa3ed9f70c1a3
Yi Zheng <yi.zheng.oulu@gmail.com>
2013-12-10 16:09:41 +0800

    Create a receiver thread for image proxy

--- img_proxy.py
+++ img_proxy.py
@@ -11,11 +11,31 @@ import time
 import sys,  getopt
 
 #current implement tation for image channel just support one client
-class MyImgBaseRequestHandlerr(StreamRequestHandler):
-    def handle(self):
+DataReady = threading.Condition()
+
+class Receiver(threading.Thread):
+    def __init__(self, t_name, socket, queue):
+        threading.Thread.__init__(self, name= t_name)
+        self.queue = queue
+        self.socket = socket
+    def run():
         while True:
             try:
-                data = self.server.detector_socket.recv(1024)
+                data = self.socket.recv(1024)
+                self.server.queue.put(data)
+            except:
+                traceback.print_exc()
+                break
+
+class MyImgBaseRequestHandlerr(StreamRequestHandler):
+    def handle(self):
+
+        while True:
+            try:
+                data = self.server.queue.get()
+                if data == None:
+                    print "Img Sender data is None quit\n"
+                    break
                 self.request.send(data)
                 #print "#"
                 if self.server.stopped:
@@ -25,11 +45,11 @@ class MyImgBaseRequestHandlerr(StreamRequestHandler):
                 break
 
 class ImgTCPServer(ThreadingTCPServer):
-    def __init__(self, service_addr, handler, detector_socket, listener):
+    def __init__(self, service_addr, handler, listener, queue):
         ThreadingTCPServer.__init__(self, service_addr, handler)
-        self.detector_socket = detector_socket
         self.stopped = False
         self.listener = listener
+        self.queue = queue
 
     def serve_forever(self):
         while not self.stopped:
@@ -37,7 +57,7 @@ class ImgTCPServer(ThreadingTCPServer):
 
     def force_stop (self):
         print "ImgTCPServer force quit"
-        self.detector_socket.settimeout(1)
+        self.queue.put(None)
         self.stopped = True
         self.detector_socket.close ()
         self.server_close()
@@ -49,6 +69,7 @@ class ImgProxy(threading.Thread):
         self.detector_img_port = detector_img_port
         self.service_port = service_port
         self.listener = listener
+        self.queue = Queue()
 
     def connect(self):
         detector_addr = (self.detector_ip, self.detector_img_port)
@@ -64,6 +85,8 @@ class ImgProxy(threading.Thread):
             self.detector_socket.connect(detector_addr)
             print "ImgProxy:: detector connect successful"
             self.listener.set_detector_connected(True)
+            self.receiver = Receiver("receiver", self.detector_socket, self.queue)
+
         except socket.error, msg:
 
             print "ImgProxy:: detector connect failed:[ERROR] %s\n" % msg[1]
@@ -73,10 +96,12 @@ class ImgProxy(threading.Thread):
         service_addr = ('', self.service_port)
 
         #start service
-        self.server = ImgTCPServer(service_addr, MyImgBaseRequestHandlerr, self.detector_socket, self.listener)
+        self.server = ImgTCPServer(service_addr, MyImgBaseRequestHandlerr, self.detector_socket, self.listener, self.queue)
         self.server.serve_forever()
 
     def stop(self):
+        #close socket should cause exception on the receiver
+        self.detector_socket.close()
         self.server.force_stop()
 
 def start_proxy(detector_ip, detector_cmd_port, service_port,listener):
